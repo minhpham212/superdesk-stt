f7c85755162020cb061775e41970a1598f35a523
Minh Pham <minh.pham@idsolutions.com.vn>
2025-08-17 09:51:19 +0700

    implement task STT-95 TT NINJS parser

--- server/settings.py
+++ server/settings.py
@@ -141,6 +141,7 @@ INSTALLED_APPS = [
     "stt.search_providers.newshub",
     "stt.ai_proxy",
     "stt.paivalista_filters",
+    "stt.stt_tt_new_parse_ninjs",
 ]
 
 # enable legal archive is enabled
